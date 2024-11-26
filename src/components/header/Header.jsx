eb5614d838ca98f3acd56339f0cc5b2841ae9767
miriamtruiolo <miriamtruiolo@gmail.com>
2024-11-26 17:39:15 +0100

    feat css and header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello</h5>
+        <h1>Maria Cristina Lia</h1>
+        <h5 className="text-light"> Personal Trainer</h5>
+        <CTA/>
+
+        <div className='me'>
+          <img src={ME} alt='me'/>
+        </div>
+
+        <a href='#contact' className='scroll_down'> Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
