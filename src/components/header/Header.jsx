5e0c882351888fd438af7a64ee9aac910d942e87
miriamtruiolo <miriamtruiolo@gmail.com>
2024-12-03 12:46:25 +0100

    fix header text css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,23 +3,29 @@ import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
 import HeaderSocials from './HeaderSocials'
+import Logo from '../../assets/logo.png'
 
 const Header = () => {
   return (
     <header>
       <div className="container header_container">
-        <h5>Hello</h5>
-        <h1>MHealthy Fit</h1>
-        <h5 className="text-light"> Personal Trainer</h5>
+      <img src={Logo} alt="Logo" className="container_logo"/>
+        <h1 className='text-primary'>MHealthy Fit</h1>
+        <h5 className="text-secondary"> Sii la tua versione più forte e salutare!
+        </h5>
         <CTA/>
         <HeaderSocials/>
 
         <div className='me'>
           <img src={ME} alt='me'/>
         </div>
-
+        <div className='text-light'>
+        <p>Inizia il tuo percorso alla scoperta delle tue incredibili capacità. <br/>Impariamo insieme, secondo le tue esigenze, a prenderci cura del tuo corpo, della tua mente e a perseguire nuovi traguardi, ritrovando il potenziale inespresso che conservi dentro di te.</p>
+      </div>
+      <div className='linea'></div>
         <a href='#contact' className='scroll_down'> Scroll Down</a>
       </div>
+      
     </header>
   )
 }
